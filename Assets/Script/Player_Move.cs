78f30a5a50ecaa7555204cbb1c97ab5a5af484eb
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-02-01 16:20:08 +0530

    23 complete

---
+++ Assets/Script/Player_Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(horizontal, vertical);
+        rb.velocity = direction * speed*Time.deltaTime;
+    }
+}
